0931438cd3cf6358cc0a84c1ae4e31c8834de615
Tegemea Lema <lemajunior@hotmail.com>
2023-08-26 16:01:35 +0300

    basic layout done

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,16 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-3">
+        side navigation
+      </div>
+      <div class="col-9">
+        <RouterView />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
