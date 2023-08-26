590b2a579623e0b20f46f1a11d0921729cc64d23
Tegemea Lema <lemajunior@hotmail.com>
2023-08-26 18:14:09 +0300

    geolocation tested & working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,15 @@
 <script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords }) => console.log(coords),
+      error => console.log(error),
+      { maximumAge: 300000, timeout: 10000, enableHighAccuracy: false }
+    )
+  }
+})
 </script>
 
 <template>
