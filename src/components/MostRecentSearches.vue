6851441d8d853e880e4bd4b562d63b62ff75f7b5
Tegemea Lema <lemajunior@hotmail.com>
2023-08-27 19:05:11 +0300

    most recent search working

---
+++ src/components/MostRecentSearches.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+const { locations } = defineProps(['locations'])
+
+</script>
+
+<template>
+    <div v-if="locations.length">
+        <h5 class="font-thin">Most Recent Searches</h5>
+        <hr />
+        <ul class="list-group list-group-flush">
+            <li v-for="location in locations" class="list-group-item">{{ location.name }}</li>
+        </ul>
+    </div>
+</template>
--- src/components/sidebar/Layout.vue
+++ src/components/sidebar/Layout.vue
@@ -1,6 +1,12 @@
 <script setup>
 import SiteTitle from './SiteTitle.vue'
 import LinksToPages from './Links.vue'
+import MostRecentSearches from '../MostRecentSearches.vue';
+import { useLocationStore } from '../../stores/locations';
+import { storeToRefs } from 'pinia'
+
+const locationStore = useLocationStore()
+const { locationsHistory } = storeToRefs(locationStore)
 </script>
 
 <template>
@@ -12,13 +18,13 @@ import LinksToPages from './Links.vue'
                 </div>
             </div>
             <div class="card mb-3">
-                <div class="card-body">
-                    current location & weather info
-                </div>
-            </div>
-            <div class="card mb-3">
                 <div class="card-body p-0">
                     <LinksToPages />
+                </div>
+            </div>
+            <div v-if="locationsHistory.length" class="card mb-3">
+                <div class="card-body">
+                    <MostRecentSearches :locations="locationsHistory" />
                 </div>
             </div>
         </div>
